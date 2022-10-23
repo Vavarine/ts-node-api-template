48829ee85beee4def021a0f28388ac3490ce1457
Evailson Barbosa <evailson@agenciaeplus.com.br>
2022-10-23 12:03:26 -0300

    chore: prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: true,
+  printWidth: 90,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  endOfLine: 'auto'
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import * as dotenv from "dotenv";
-import express from "express";
+import * as dotenv from 'dotenv';
+import express from 'express';
 
-import { logInfo } from "./utils/log";
+import { logInfo } from './utils/log';
 
 dotenv.config();
 const app = express();
 
-app.get("/", (_req, res) => {
-  res.send("Hello World");
+app.get('/', (_req, res) => {
+	res.send('Hello World');
 });
 
 app.listen(process.env.PORT ?? 3333, () => {
-  logInfo("server", `running on port ${process.env.PORT ?? 3333}`);
+	logInfo('server', `running on port ${process.env.PORT ?? 3333}`);
 });
